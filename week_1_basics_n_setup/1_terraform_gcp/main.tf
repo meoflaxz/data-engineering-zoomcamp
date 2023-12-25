c23945f90bf2b2b20e379f389ec44bf5c28d5c22
Meor Hasyim <meorhashim@gmail.com>
2023-12-25 18:21:31 +0800

    Update main.tf

--- week_1_basics_n_setup/1_terraform_gcp/main.tf
+++ week_1_basics_n_setup/1_terraform_gcp/main.tf
@@ -1,6 +1,9 @@
 terraform {
     required_version = "> 1.0"
-    backend "local" {}
+     # The local backend stores the Terraform state file on the local disk of the machine where Terraform commands are executed.
+     # Can change from "local" to "gcs" (for google) or "s3" (for aws), if you would like to preserve your tf-state online
+     backend "local" {}
+  required_providers {
     required_providers {
         google = {
             source = "hashicorp/google"
@@ -13,13 +16,39 @@ provider "google" {
     region = var.region
 }
 
+# Data Lake Bucket
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
 resource "google_storage_bucket" "data-lake-bucket" {
+     # Concatenating DL bucket & Project name for unique naming
     name        = "${local.data_lake_bucket}_${var.project}"
     location    = var.region
+
+    # Optional, but recommended settings:
+    storage_class = var.storage_class
+    uniform_bucket_level_access = true
+
+    versioning {
+        enabled = true
+    }
+
+    lifecycle_rule {
+        action {
+            type = "Delete"
+        }
+        condition {
+            age = 30 // days
+        }
+
+        force_destroy = true
+    }
 }
 
+# DWH
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
 resource "google_bigquery_dataset" "dataset" {
     dataset_id  = var.BQ.dataset
     project     = var.project 
     location    = var.region
+}
+
 }
