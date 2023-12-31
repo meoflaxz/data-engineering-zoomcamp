33b8a4134cd87abec0163c4eb29b086659a4dca4
Meor Hasyim <meorhashim@gmail.com>
2023-12-31 23:17:18 +0800

    update comment on external data config

--- week_4_analytics/main.tf
+++ week_4_analytics/main.tf
@@ -33,6 +33,7 @@ resource "google_bigquery_table" "table" {
     dataset_id                  = google_bigquery_dataset.dataset.dataset_id
     table_id                    = "example_table"
 
+# checking later for data pulling from external sources
     # external_data_configuration {
     #     autodetect              = true
     #     source_format           = "CSV"
